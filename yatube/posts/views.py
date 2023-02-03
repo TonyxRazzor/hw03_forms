4eda7e091fe1ed78e31467c0b47df8496aef9a5f
Tony Razzor <tonyxrazzor@yandex.ru>
2023-02-03 13:34:16 +0300

    Тест проекта №3: ver 1.2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import PostForm
 from .models import Group, Post, User
-from django.contrib.auth.decorators import login_required
+
 
 DEF_POST = 10
 
@@ -81,8 +82,7 @@ def post_edit(request, post_id):
         return redirect('posts:post_detail', post_id=post_id)
     form = PostForm(request.POST or None, instance=post)
     if form.is_valid():
-        post = form.save(commit=False)
-        post.save()
+        form.save()
         return redirect('posts:post_detail', post_id=post_id)
     context = {
         'form': form,
